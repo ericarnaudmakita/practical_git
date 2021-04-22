2d471fae396a29e08c0fe597d23f5eac2ced7132
Eric Makita <eric.makita-extern@renault.com>
2021-04-22 16:06:06 +0200

    completed the all months

--- getMonthName.py
+++ getMonthName.py
@@ -1,7 +1,8 @@
 def getMonthName():
     """ this function capitalizes the each month in the months liste
     with the list comprehension"""
-    months = ['january', 'february','march', 'april', 'may', 'june']
+    months = ['january', 'february','march', 'april', 'may', 'june', 'july', 'aout', 'september', 
+    'october', 'november', 'december']
     converted_months = [x.title() for x in months]
     
     return converted_months
