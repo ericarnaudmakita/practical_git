17e22e0cc8c732777ec564bd83ca9a485ca78d98
Eric Makita <eric.makita-extern@renault.com>
2021-04-22 14:31:56 +0200

    add apitalization to getMonthName

--- getMonthName.py
+++ getMonthName.py
@@ -1,3 +1,6 @@
-def getMonthName(place):
- months = ['january', 'february','march', 'april']
+def getMonthName():
+    months = ['january', 'february','march', 'april']
+    converted_months = [x.title() for x in months]
+    
+    return converted_months
  
