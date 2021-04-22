1175629ec34b2d2b883d45dfc886fc52d3339bed
Eric Makita <eric.makita-extern@renault.com>
2021-04-22 04:29:12 +0200

    added lignes from commit our changes

--- practical_git.py
+++ practical_git.py
@@ -12,3 +12,28 @@ rm -rf .git/
 
 # push an existing repository from the command line
 git remote add origin https://github.com/ericarnaudmakita/practical_git.git
+
+# check if we setup correctly the remote repo
+git remote -v
+
+# Now, in case we have to clone a remote repo, we do the following
+# git clone follows by the link of the repos, example:
+git clone https://github.com/ericarnaudmakita/practical_git.git
+
+# Add or stage all the changes that we made
+git add -A
+
+# we can check statuts of our branch
+git status
+
+# Commit our changes
+git commit - m "add some lignes"
+
+# check the status of our repos
+git status
+
+# push the first commit 
+git push --set-upstream origin master
+
+# check the status of our repos
+git status
