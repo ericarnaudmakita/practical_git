65dadb286e2e15aeff01357c7001aa2d71a8278c
Eric Makita <eric.makita-extern@renault.com>
2021-04-22 04:18:38 +0200

    add some new lines

---
+++ practical_git.py
@@ -0,0 +1,14 @@
+# initialize new repo
+git init
+
+# check what's inside the repo
+ls -a
+
+# remove git from a project
+rm -rf .git/
+
+#the best practice will be to use the 
+# the same name of the project folder as repository name
+
+# push an existing repository from the command line
+git remote add origin https://github.com/ericarnaudmakita/practical_git.git
