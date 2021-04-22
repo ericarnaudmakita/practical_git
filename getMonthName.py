de4f9fb0d17da890744397fde0f683a76e91f760
Eric Makita <eric.makita-extern@renault.com>
2021-04-22 14:40:16 +0200

    add one line of comment

--- getMonthName.py
+++ getMonthName.py
@@ -1,6 +1,9 @@
 def getMonthName():
+
     months = ['january', 'february','march', 'april']
     converted_months = [x.title() for x in months]
+
+    # use the list comprehension for such things
     
     return converted_months
  
